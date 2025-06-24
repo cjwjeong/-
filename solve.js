764b57153d575a723501f19e7fc8515919e59307
정성원 <jwjeong2187@snu.ac.kr>
2025-06-24 21:26:24 +0900

    문제 풀이 내용 추가

--- solve.js
+++ solve.js
@@ -1,3 +1,41 @@
-function test(){
-  pass
-}
+//1번
+// let nums = [100, 200, 300, 400, 500];
+// nums.splice(3, 2);
+// console.log(nums);
+
+//2번
+// let arr = [200, 100, 300];
+// arr.splice(2, 0, 10000)
+// console.log(arr);
+
+//9번
+// let year = '2019';
+// let month = '04';
+// let day = '26';
+// let hour ='11';
+// let minute = '34';
+// let second = '27';
+
+// let result = [year, month, day].join("/") + " " + [hour, minute, second].join(":")
+// console.log(result)
+
+//문제 10번
+// let num = 10; //여기다 숫자
+// let arr = [];
+
+// for(i = num; i!=0; i--){
+//   arr.push(" ".repeat(num - i)+ "*".repeat(2 * i - 1) + " ".repeat(num - i));
+// };
+
+// arr.reverse()
+
+// for(tree of arr){
+//   console.log(tree);
+// }
+
+
+
+
+
+
+
